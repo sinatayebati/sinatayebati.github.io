094130cd5ae24c71570a105bcc3992b81675bd09
sinatayebati <sinatayebati@gmail.com>
2023-08-23 00:30:28 -0500

    commit #17 header toggler update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -48,7 +48,8 @@ const Header = () => {
                         </li>                       
                     </ul>
 
-                    <i class="uil uil-times nav__close"></i>
+                    <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+
                 </div>
 
                 <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
