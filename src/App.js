dc9d83c102c7f0ff0a16aad66a702c06235b7b67
sinatayebati <sinatayebati@gmail.com>
2023-08-23 22:16:53 -0500

    commit #18 home and social sections added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./App.css"
 import Header from './components/header/header';
+import Home from './components/home/home';
 
 const App = () => {
     return (
         <>
         <Header />
+
+        <main className='main'>
+            <Home />
+        </main>
         </>
     )
 }
