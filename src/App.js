b9cb3bc8ff16ee747d1f98e7a1b42ed8017a06a2
sinatayebati <sinatayebati@gmail.com>
2023-08-30 22:45:08 -0500

    commit #37 skills initiated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import Header from './components/header/header';
 import Home from './components/home/home';
 import About from './components/about/about';
+import Skills from './components/skills/skills';
 
 const App = () => {
     return (
