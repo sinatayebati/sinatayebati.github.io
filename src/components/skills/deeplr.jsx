edd70d85a51f3bceec3d6d425fad61b6bdf30c12
sinatayebati <sinatayebati@gmail.com>
2023-09-01 22:13:02 -0500

    commit #43 skills update

--- src/components/skills/deeplr.jsx
+++ src/components/skills/deeplr.jsx
@@ -7,42 +7,43 @@ const Deeplr = () => {
 
             <div className="skills__box">
                 <div className="skills__group">
-                    <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-
-                    <div>
-                        <h3 className="skills__name">PyTorch</h3>
-                        <span className="skills__level">Advanced</span>
-                    </div>
-                    </div>
 
                     <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                        <i class='bx bx-badge-check'></i>
 
-                    <div>
-                        <h3 className="skills__name">Pyro</h3>
-                        <span className="skills__level">Advanced</span>
-                    </div>
+                        <div>
+                            <h3 className="skills__name">PyTorch</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
                     </div>
 
                     <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                        <i class='bx bx-badge-check'></i>
 
-                    <div>
-                        <h3 className="skills__name">TensorFlow</h3>
-                        <span className="skills__level">Intermediate</span>
+                        <div>
+                            <h3 className="skills__name">Pyro</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
                     </div>
 
                     <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                        <i class='bx bx-badge-check'></i>
 
-                    <div>
-                        <h3 className="skills__name">Scikit-Learn</h3>
-                        <span className="skills__level">Advanced</span>
+                        <div>
+                            <h3 className="skills__name">TensorFlow</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
                     </div>
 
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+
+                        <div>
+                            <h3 className="skills__name">Scikit-Learn</h3>
+                            <span className="skills__level">Advanced</span>
+                        </div>
                     </div>
-                    </div>
+
                 </div>
 
                 <div className="skills__group">
--- src/components/skills/frontend.jsx
+++ src/components/skills/frontend.jsx
@@ -7,41 +7,43 @@ const Frontend = () => {
 
         <div className="skills__box">
             <div className="skills__group">
-                <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills__name">HTML</h3>
-                    <span className="skills__level">Advanced</span>
-                </div>
-                </div>
 
                 <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills__name">CSS</h3>
-                    <span className="skills__level">Advanced</span>
-                </div>
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">HTML</h3>
+                        <span className="skills__level">Advanced</span>
+                    </div>
                 </div>
 
                 <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
+                    <i class='bx bx-badge-check'></i>
 
-                <div>
-                    <h3 className="skills__name">JavaScript</h3>
-                    <span className="skills__level">Intermediate</span>
+                    <div>
+                        <h3 className="skills__name">CSS</h3>
+                        <span className="skills__level">Advanced</span>
+                    </div>
                 </div>
 
                 <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
+                    <i class='bx bx-badge-check'></i>
 
-                <div>
-                    <h3 className="skills__name">React</h3>
-                    <span className="skills__level">Intermediate</span>
+                    <div>
+                        <h3 className="skills__name">JavaScript</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
                 </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">React</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
                 </div>
-                </div>
+
             </div>
 
             <div className="skills__group">
