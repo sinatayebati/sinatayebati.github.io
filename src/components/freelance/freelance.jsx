286501f56c28399ddf2f021ca384483f3310b6a0
sinatayebati <sinatayebati@gmail.com>
2023-09-07 22:45:36 -0500

    commit #51 freelance updates

--- src/components/freelance/freelance.jsx
+++ src/components/freelance/freelance.jsx
@@ -7,10 +7,11 @@ const Freelance = () => {
             <span className="section__subtitle">What I offer</span>
 
             <div className="frelance__container container grid">
+
                 <div className="freelance__content">
                     <div>
                         <i className="uil uil freelance__icon"></i>
-                        <h3 className="freelance__title"></h3>
+                        <h3 className="freelance__title">ML <br /> Developer</h3>
                     </div>
 
                     <span className="freelance__button">View More <i className="uil uil-arrow-right freelance__button-icon"></i></span>
@@ -27,14 +28,21 @@ const Freelance = () => {
                                 <li className="freelance__modal-freelance">
                                     <i className="uil uil-check-circle freelance__modal-icon"></i>
                                     <p className="freelance__modal-info">
-                                        I develop deep learning and AI models.
+                                        Deep learning and AI development
                                     </p>
                                 </li>
 
                                 <li className="freelance__modal-freelance">
                                     <i className="uil uil-check-circle freelance__modal-icon"></i>
                                     <p className="freelance__modal-info">
-                                        I develop deep learning and AI models.
+                                        Frontend development
+                                    </p>
+                                </li>
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Backend development
                                     </p>
                                 </li>
 
@@ -42,6 +50,93 @@ const Freelance = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="freelance__content">
+                    <div>
+                        <i className="uil uil freelance__icon"></i>
+                        <h3 className="freelance__title">Frontend <br /> Developer</h3>
+                    </div>
+
+                    <span className="freelance__button">View More <i className="uil uil-arrow-right freelance__button-icon"></i></span>
+
+                    <div className="freelance__modal">
+                        <div className="freelance__modal-content">
+                            <i className="uil uil-times freelance__modal-close"></i>
+
+                            <h3 className="freelance__modal-title"></h3>
+                            <p className="freelance__modal-description">Freelance services and collaborations with more than 2 years of experiennce.</p>
+
+                            <ul className="freelance__modal-freelance grid">
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Deep learning and AI development
+                                    </p>
+                                </li>
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Frontend development
+                                    </p>
+                                </li>
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Backend development
+                                    </p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="freelance__content">
+                    <div>
+                        <i className="uil uil freelance__icon"></i>
+                        <h3 className="freelance__title">Backend <br /> Developer</h3>
+                    </div>
+
+                    <span className="freelance__button">View More <i className="uil uil-arrow-right freelance__button-icon"></i></span>
+
+                    <div className="freelance__modal">
+                        <div className="freelance__modal-content">
+                            <i className="uil uil-times freelance__modal-close"></i>
+
+                            <h3 className="freelance__modal-title"></h3>
+                            <p className="freelance__modal-description">Freelance services and collaborations with more than 2 years of experiennce.</p>
+
+                            <ul className="freelance__modal-freelance grid">
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Deep learning and AI development
+                                    </p>
+                                </li>
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Frontend development
+                                    </p>
+                                </li>
+
+                                <li className="freelance__modal-freelance">
+                                    <i className="uil uil-check-circle freelance__modal-icon"></i>
+                                    <p className="freelance__modal-info">
+                                        Backend development
+                                    </p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </section>
