f45f5c8c58d144727d2ab259eaa29e4564f0727b
sinatayebati <sinatayebati@gmail.com>
2023-09-11 23:52:13 -0500

    commit #71 qualification updates

--- src/components/qualification/qualification.jsx
+++ src/components/qualification/qualification.jsx
@@ -6,6 +6,20 @@ const Qualification = () => {
         <section className="qualification section">
             <h2 className="section__title">Qualification</h2>
             <span className="section__subtitle">My personal journey</span>
+
+            <div className="qualification__container container">
+                <div className="qualification__tabs">
+
+                    <div className="qualification__button button--flex">
+                        <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                    </div>
+
+                    <div className="qualification__button button--flex">
+                        <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                    </div>
+
+                </div>
+            </div>
         </section>
     )
 }
