ddf810510a45fae1e3affb55b6eba8c5141d3fd1
sinatayebati <sinatayebati@gmail.com>
2023-09-16 23:03:35 -0500

    commit #82 publication updates

--- src/components/publication/publication.jsx
+++ src/components/publication/publication.jsx
@@ -4,30 +4,36 @@ import "./publication.css";
 const Publication = () => {
     return (
         <section className="publications section" id="publication">
-            <h2 className="section__title">Publication</h2>
-            <span className="section__subtitle">My research papers</span>
+            <h2 className="section__title">Publications</h2>
+            <span className="section__subtitle">My Research Papers</span>
 
-            <div className="publication__container container grid">
+            <div className="publication__container container">
                 <div className="publication__content">
-                    <ul>
-                        <li>
-                            <div className="publication">
-                                <div className="publication__title">
-                                    A hybrid machine learning framework for clad characteristics prediction in metal additive manufacturing
-                                </div>
-                                <div className="publication__authors">
-                                    S Tayebati, KT Cho - arXiv preprint arXiv:2307.01872, 2023 - arxiv.org
-                                </div>
-                                <a href="https://arxiv.org/abs/2307.01872" className="publication__link" target="blank">
-                                    <div class="link">Download</div>
-                                </a> 
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                
-            </div>
+                    <div className="publication">
+                        <div className="publication__title">
+                            STARNet: Sensor Trustworthiness and Anomaly Recognition via Approximated Likelihood Regret for Robust Edge Autonomy
+                        </div>
+                        <div className="publication__authors">
+                            Darabi N, Tayebati T, Sureshkumar S, Ravi S, Tulabandhula T, Trivedi A.R
+                        </div>
+                        <a href="#" className="publication__link" target="_blank">
+                            Download
+                        </a> 
+                    </div>
 
+                    <div className="publication">
+                        <div className="publication__title">
+                            A Hybrid Machine Learning Framework for Clad Characteristics Prediction in Metal Additive Manufacturing
+                        </div>
+                        <div className="publication__authors">
+                            S Tayebati, KT Cho - arXiv preprint arXiv:2307.01872, 2023 - arxiv.org
+                        </div>
+                        <a href="https://arxiv.org/abs/2307.01872" className="publication__link" target="_blank">
+                            Download
+                        </a> 
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
