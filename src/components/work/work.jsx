ce58ce1424a3b20fc56d24323264384a74be7275
sinatayebati <sinatayebati@gmail.com>
2023-09-18 23:13:40 -0500

    commit #85 portfolio section

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./work.css"
+import Works from "./works";
 
 const Work = () => {
     return (
-        <div>Work</div>
+        <section className="word section" id="portfolio">
+            <h2 className="section__title">Portfolio</h2>
+            <span className="section__subtitle">Most Recent Works</span>
+
+            <Works />
+        </section>
     )
 }
 
---
+++ src/components/work/works.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./work.css"
+
+const Works = () => {
+    return (
+        <div>Works</div>
+    )
+}
+
+export default Works
