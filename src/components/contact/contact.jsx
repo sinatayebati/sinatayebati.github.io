eb02a669a1b6a2cc31f643fe8806318e5b36e513
sinatayebati <sinatayebati@gmail.com>
2023-10-02 00:25:54 -0500

    commit #112 contact section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
 
                     <div className="contact__info">
                         <div className="contact__card">
-                            <i className="bx bx contact__card-icon"></i>
+                            <i className="bx bx-mail-send contact__card-icon"></i>
 
                             <h3 className="contact__card-title">Email</h3>
                             <span className="contact__card-data">stayeb3@uic.edu</span>
@@ -25,7 +25,7 @@ const Contact = () => {
                         </div>
 
                         <div className="contact__card">
-                            <i className="bx bx contact__card-icon"></i>
+                            <i className="bx bx-map contact__card-icon"></i>
 
                             <h3 className="contact__card-title">Location</h3>
                             <span className="contact__card-data">Chicag0, IL, U.S.</span>
@@ -37,10 +37,10 @@ const Contact = () => {
                         </div>
 
                         <div className="contact__card">
-                            <i className="bx bx contact__card-icon"></i>
+                            <i className="bx bxs-hand-right contact__card-icon"></i>
 
-                            <h3 className="contact__card-title">Wait!</h3>
-                            <span className="contact__card-data">Try submitting your inquiry, it actually works!</span>
+                            <h3 className="contact__card-title">Try it</h3>
+                            <span className="contact__card-data">It actually works!</span>
 
                             <a href="#" className="contact__button">
                                 Write me{" "}
@@ -52,6 +52,39 @@ const Contact = () => {
 
                 <div className="contact__content">
                     <h3 className="contact__title">Write me your project!</h3>
+
+                    <form action="#" className="contact__form">
+                        <div className="contact__form-div">
+                            <label htmlFor="#" className="contact__form-tag">Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                className="contact__form-input"
+                                placeholder="Insert your name"
+                            />
+                        </div>
+
+                        <div className="contact__form-div">
+                            <label htmlFor="#" className="contact__form-tag">Mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="contact__form-input"
+                                placeholder="Insert your email"
+                            />
+                        </div>    
+
+                        <div className="contact__form-div">
+                            <label htmlFor="#" className="contact__form-tag">Project</label>
+                            <textarea 
+                            name="project" 
+                            cols="30" 
+                            rows="10"
+                            className="contact__form-input"
+                            placeholder="Write your project"
+                            ></textarea>
+                        </div>                          
+                    </form>
                 </div>
             </div>
             
