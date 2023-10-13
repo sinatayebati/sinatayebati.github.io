f10ede302a14a02c7d5da0e2a0f56c3aef8471cf
sinatayebati <sinatayebati@gmail.com>
2023-10-12 22:57:52 -0500

    commit #118 contact me style updates

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -75,7 +75,7 @@ const Contact = () => {
                             />
                         </div>    
 
-                        <div className="contact__form-div">
+                        <div className="contact__form-div contact__form-area">
                             <label htmlFor="#" className="contact__form-tag">Project</label>
                             <textarea 
                             name="project" 
