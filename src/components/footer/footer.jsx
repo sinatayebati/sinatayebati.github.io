58ebf88b91691f63cf0ac9ff66c5394d18a12daa
sinatayebati <sinatayebati@gmail.com>
2023-10-21 17:06:00 -0500

    commit #125 footer style updates

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
                 </div>
 
                 <span className="footer__copy">
-                    &#169; All rights reserved
+                    &#169; Sina Tayebati. All rights reserved
                 </span>
             </div>
         </footer>
